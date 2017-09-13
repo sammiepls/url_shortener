776be9e21c4af2117447f261a891fec149925a9e
sammiepls <samantha.lhe@gmail.com>
2017-09-13 17:31:31 +0800

    Add validation to urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,21 @@
+require 'securerandom'
+require 'uri'
+
 class Url < ApplicationRecord
+  before_create :shorten
+  validates :ori_url, presence: true
+  validate :url_format
+
+
+	def shorten
+    self.short_url = SecureRandom.hex(5)
+  end
+
+  def url_format
+    begin
+      errors.add(:url, "Invalid url") unless URI(self.ori_url).is_a?(URI::HTTP)
+    rescue URI::InvalidURIError
+      errors.add(:url, "Invalid url")
+    end
+  end
 end
